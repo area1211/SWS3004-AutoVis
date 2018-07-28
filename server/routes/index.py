1c97595bf63294d6055f7eec827e259062d8ec6d
junginho <area1211@naver.com>
2018-07-28 15:04:04 +0800

    first develop front end.

--- server/routes/index.py
+++ server/routes/index.py
@@ -1,9 +1,19 @@
 from server import app
-from flask import render_template
+from flask import render_template, request
 
-@app.route('/')
+@app.route('/index')
 def hello_world():
     return app.send_static_file('index.html')
+
+@app.route('/', methods=['POST', 'GET'])
+def chart_form():
+    print("chart_form logic")
+    if request.method == 'POST':
+        print("Seleted indicator : ", request.form['indicator-1'], request.form['indicator-2'])
+        print("Seleted country : ", request.form['country-1'], request.form['country-2'], request.form['country-3'])
+
+        print(request.form['start-year'], request.form['end-year']);
+    return app.send_static_file('chart_form.html')
 
 @app.errorhandler(404)
 @app.route("/error404")
